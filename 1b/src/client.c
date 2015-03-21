3dd26ac36a6203ba4fd6b9cd40c15c86801cd494
smaug95 <johannes.vass@gmail.com>
2015-03-21 13:05:52 +0100

    Task 1b: initial commit

---
+++ 1b/src/client.c
@@ -0,0 +1,35 @@
+/**
+ * @file client.c
+ * @author Johannes Vass <e1327476@student.tuwien.ac.at>
+ * @date 21.03.2015
+ *
+ * @brief 
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+
+/* === Constants === */
+
+/* === Global Variables === */
+
+/* === Type Definitions === */
+
+/* === Prototypes === */
+
+/* === Implementations === */
+
+/**
+ * @brief Program entry point
+ * @param argc The argument counter
+ * @param argv The argument vector
+ * @return {einfügen}
+ */
+int main(int argc, char *argv[])
+{
+	
+	return EXIT_SUCCESS;
+}
---
+++ 1b/src/test.c
@@ -0,0 +1,19 @@
+/**
+ * @file test.c
+ * @author Johannes Vass <e1327476@student.tuwien.ac.at>
+ * @date 21.03.2015
+ *
+ * @brief This application is used to test the functionality of the mastermind client-server application.
+ **/
+
+/**
+ * @brief Program entry point
+ * @param argc The argument counter
+ * @param argv The argument vector
+ * @return {einfügen}
+ */
+int main(int argc, char *argv[])
+{
+	
+	return EXIT_SUCCESS;
+}
