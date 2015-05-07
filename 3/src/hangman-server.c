0795b29d0a7562d744e1bbe5707bde9e9765fab3
smaug95 <johannes.vass@gmail.com>
2015-05-07 16:40:17 +0200

    Initial commit for exercise 3

---
+++ 3/src/hangman-server.c
@@ -0,0 +1,94 @@
+/**
+ * @file hangman-server.c
+ * @author Johannes Vass <e1327476@student.tuwien.ac.at>
+ * @date 10.05.2015
+ * 
+ * @brief This module behaves as a server in the hangman game.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <limits.h>
+
+/* === Constants === */
+
+
+/* === Macros === */
+#ifdef _ENDEBUG
+#define DEBUG(...) do { fprintf(stderr, __VA_ARGS__); } while(0)
+#else
+#define DEBUG(...)
+#endif
+
+
+/* === Global Variables === */
+
+/* Name of the program */
+static const char *progname = "hangman-server"; /* default name */
+
+
+/* === Prototypes === */
+
+/**
+ * @brief terminate program on program error
+ * @param exitcode exit code
+ * @param fmt format string
+ */
+static void bail_out(int exitcode, const char *fmt, ...);
+
+/**
+ * @brief Signal handler
+ * @param sig Signal number catched
+ */
+static void signal_handler(int sig);
+
+/**
+ * @brief free allocated resources
+ */
+static void free_resources(void);
+
+
+/* === Implementations === */
+
+static void bail_out(int exitcode, const char *fmt, ...)
+{
+    va_list ap;
+
+    (void) fprintf(stderr, "%s: ", progname);
+    if (fmt != NULL) {
+        va_start(ap, fmt);
+        (void) vfprintf(stderr, fmt, ap);
+        va_end(ap);
+    }
+    if (errno != 0) {
+        (void) fprintf(stderr, ": %s", strerror(errno));
+    }
+    (void) fprintf(stderr, "\n");
+
+    free_resources();
+    exit(exitcode);
+}
+
+static void free_resources(void)
+{
+    /* clean up resources */
+    DEBUG("Shutting down server\n");
+    // TODO: Implement
+}
+
+/**
+ * @brief Program entry point
+ * @param argc The argument counter
+ * @param argv The argument vector
+ * @return TODO
+ */
+int main(int argc, char *argv[])
+{
+	return EXIT_SUCCESS;
+}
